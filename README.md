858cd03b33ea946e74c51c966bd15bf1584fd765
Artyom Moiseev <42650429+ArtyomMoiseev@users.noreply.github.com>
2022-04-11 17:33:11 +0500

    Add homework
    Add homework

---
+++ README.md
@@ -0,0 +1,12 @@
+# Spring Boot web (hw 6)
+## Демонстрация работы
+`127.0.0.1:8080/getRequestHeaders`
+![](md_images/get.png)
+`127.0.0.1:8080/addIdToJson`
+![](md_images/post.png)
+Ловим exception, при сломанном json-е
+![](md_images/post_crack.png)
+## Запуск
+Выполните следующие команды:
+`mvn package`
+`mvn spring-boot:run`
---
+++ src/main/java/com/example/skb_course_hw6/SkbCourseHw6Application.java
@@ -0,0 +1,13 @@
+package com.example.skb_course_hw6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkbCourseHw6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkbCourseHw6Application.class, args);
+    }
+
+}
